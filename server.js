bef5023f1c618d300e182d347d4351b2a9f7a794
ssiren <ssiren@kth.se>
2020-02-06 22:07:49 +0100

    Implement server commit status
    Closes #6

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const firebase_controller = require('./controllers/firebase_controller')
+var helper = require('./webhook_functions.js')
+
+const app = express()
+const port = 3001
+const fs = require('fs');
+const executeBuild = require('./executeBuild.js')
+const shell = require('shelljs')
+
+app.use(bodyParser.json())
+
+/*
+    Handles web-hooks POST request.
+*/
+app.post('/', (req, res) => {
+	var body = req.body;
+    const path = '/home/ubuntu/ci';
+    shell.cd(path);
+	shell.exec('rm -drf ContinuousIntegration');
+
+	shell.exec('git clone ' + req.body.repository.html_url);
+	
+    
+
+    console.log(body);
+	console.log(req.body.repository.id)
+
+	
+
+/*
+    Example set_status 
+    Note that status can be one of error, failure, pending, or success
+    Don't forget to enter your token to token.json and remove it when push
+*/
+    //helper.set_status(req, 'pending')
+	if (executeBuild.execute().flag == true){
+		console.log('success');
+		helper.set_status(req, 'success');
+	} else {
+		console.log('failure');
+		helper.set_status(req, 'failure')
+		console.log('failure');
+	}
+    res.send('finished' + executeBuild.execute().flag);
+	
+})
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+/*
+    Get all the repositories and the information about them.
+*/
+app.get('/repo', async (req, res) => {
+    const data = await firebase_controller.getAllRepos();
+    if(data != null) {
+        res.status(200).send(data);
+    } else {
+        res.status(500).send("data empty")
+    }
+    
+})
+
+/*
+    Get all the builds from a specific repository id.
+*/
+app.get('/repo/:id', async (req, res, next) => {
+    const data = await firebase_controller.getRepoBuilds(req.params.id);
+    if(data != null) {
+        res.status(200).send(data);
+    } else {
+        res.status(500).send("data empty")
+    }
+})
+
+/*
+    Get build from repository id and build id(head_commit.id)
+*/
+app.get('/repo/:repo_id/:build_id', async (req, res) => {
+    const data = await firebase_controller.getBuild(req.params.repo_id, req.params.build_id);
+    if(data != null) {
+        res.status(200).send(data);
+    } else {
+        res.status(500).send("data empty")
+    }
+})
+
+
+app.listen(port)
+console.log('Server is up on port ', port)
---
+++ webhook_functions.js
@@ -0,0 +1,60 @@
+const request = require('request')
+const config = require('./config_server.json')
+
+function get_sha(req) {
+
+	var sha = req.body.head_commit.id
+    
+    return sha
+}
+
+function get_url(req) {
+    var sha = get_sha(req)
+    url = 'https://api.github.com/repos/' + req.body.repository.full_name + '/statuses/' + sha
+    return url
+}
+
+function set_status(req, status) {
+
+    url = get_url(req)
+    sha = get_sha(req)
+    var repo_id = req.body.repository.id
+    var res
+	console.log(status)
+
+    status = {
+        state: status,
+        target_url: config['database'] + "/" + repo_id + + "/" + sha,
+        description: "The build is " + status,
+        context: "CI/group-7"
+    } 
+
+    var token = "Bearer " + config['token']
+	console.log(status)
+    request({
+        url: url,
+        method: "POST",
+        json: true,
+        headers: {
+            'User-Agent': 'node.js',
+            'Content-Type': 'application/json',
+            'Authorization': token
+        },
+        body: status
+    }, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            }
+            res = response
+    })
+
+
+    
+    return res
+}
+
+module.exports = {
+    get_sha,
+    get_url,
+    set_status
+}
